324a0629418921e866a7d4eb18a13dee71ec8f49
Ankita Thomas <ankithom@redhat.com>
2022-04-12 07:16:33 -0400

    e2e: fail-forward from failed CSV

--- test/e2e/fail_forward_e2e_test.go
+++ test/e2e/fail_forward_e2e_test.go
@@ -190,7 +190,101 @@ var _ = Describe("Fail Forward Upgrades", func() {
 			}).Should(BeTrue())
 		})
 	})
-	When("a CSV resource is in a failed state", func() {})
+	When("a CSV resource is in a failed state", func() {
+		var (
+			magicCatalog      MagicCatalog
+			catalogSourceName string
+			subscription      *operatorsv1alpha1.Subscription
+		)
+		BeforeEach(func() {
+			provider, err := NewFileBasedFiledBasedCatalogProvider(filepath.Join(testdataDir, failForwardTestDataBaseDir, "example-operator.v0.1.0.yaml"))
+			Expect(err).To(BeNil())
+
+			catalogSourceName = genName("mc-csv-failed-")
+			magicCatalog = NewMagicCatalog(c, ns.GetName(), catalogSourceName, provider)
+			Expect(magicCatalog.DeployCatalog(context.Background())).To(BeNil())
+
+			By("creating the testing subscription")
+			subscription = &operatorsv1alpha1.Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("%s-sub", catalogSourceName),
+					Namespace: ns.GetName(),
+				},
+				Spec: &operatorsv1alpha1.SubscriptionSpec{
+					CatalogSource:          catalogSourceName,
+					CatalogSourceNamespace: ns.GetName(),
+					Channel:                "stable",
+					Package:                "packageA",
+				},
+			}
+			Expect(c.Create(context.Background(), subscription)).To(BeNil())
+
+			By("waiting until the subscription has an IP reference")
+			subscription, err := fetchSubscription(crclient, subscription.GetNamespace(), subscription.GetName(), subscriptionHasInstallPlanChecker)
+			Expect(err).Should(BeNil())
+
+			By("waiting for the v0.1.0 CSV to report a succeeded phase")
+			_, err = fetchCSV(crclient, subscription.Status.CurrentCSV, ns.GetName(), buildCSVConditionChecker(operatorsv1alpha1.CSVPhaseSucceeded))
+			Expect(err).ShouldNot(HaveOccurred())
+
+			By("updating the catalog with a broken v0.2.0 csv")
+			brokenProvider, err := NewFileBasedFiledBasedCatalogProvider(filepath.Join(testdataDir, failForwardTestDataBaseDir, "example-operator.v0.2.0-2.yaml"))
+			Expect(err).To(BeNil())
+
+			err = magicCatalog.UpdateCatalog(context.Background(), brokenProvider)
+			Expect(err).To(BeNil())
+
+			badCSV := "example-operator.v0.2.0"
+			By("verifying the subscription has installed the current csv")
+			subscription, err = fetchSubscription(crclient, subscription.GetNamespace(), subscription.GetName(), subscriptionHasCurrentCSV(badCSV))
+			Expect(err).Should(BeNil())
+
+			By("waiting for the bad CSV to report a failed state")
+			_, err = fetchCSV(crclient, subscription.Status.CurrentCSV, ns.GetName(), csvFailedChecker)
+			Expect(err).To(BeNil())
+
+		})
+		AfterEach(func() {
+			By("removing the testing catalog resources")
+			Expect(magicCatalog.UndeployCatalog(context.Background())).To(BeNil())
+		})
+		It("eventually reports a successful state when using skip ranges", func() {
+			By("patching the catalog with a fixed version")
+			fixedProvider, err := NewFileBasedFiledBasedCatalogProvider(filepath.Join(testdataDir, "fail-forward/skip-range", "example-operator.v0.3.0.yaml"))
+			Expect(err).To(BeNil())
+
+			err = magicCatalog.UpdateCatalog(context.Background(), fixedProvider)
+			Expect(err).To(BeNil())
+
+			By("waiting for the subscription to have the example-operator.v0.3.0 status.updatedCSV")
+			subscription, err = fetchSubscription(crclient, subscription.GetNamespace(), subscription.GetName(), subscriptionHasCurrentCSV("example-operator.v0.3.0"))
+			Expect(err).Should(BeNil())
+		})
+		It("eventually reports a successful state when using skips", func() {
+			By("patching the catalog with a fixed version")
+			fixedProvider, err := NewFileBasedFiledBasedCatalogProvider(filepath.Join(testdataDir, "fail-forward/skips", "example-operator.v0.3.0.yaml"))
+			Expect(err).To(BeNil())
+
+			err = magicCatalog.UpdateCatalog(context.Background(), fixedProvider)
+			Expect(err).To(BeNil())
+
+			By("waiting for the subscription to have the example-operator.v0.3.0 status.updatedCSV")
+			subscription, err = fetchSubscription(crclient, subscription.GetNamespace(), subscription.GetName(), subscriptionHasCurrentCSV("example-operator.v0.3.0"))
+			Expect(err).Should(BeNil())
+		})
+		It("eventually reports a successful state when using replaces", func() {
+			By("patching the catalog with a fixed version")
+			fixedProvider, err := NewFileBasedFiledBasedCatalogProvider(filepath.Join(testdataDir, "fail-forward/replaces", "example-operator.v0.3.0.yaml"))
+			Expect(err).To(BeNil())
+
+			err = magicCatalog.UpdateCatalog(context.Background(), fixedProvider)
+			Expect(err).To(BeNil())
+
+			By("waiting for the subscription to have the example-operator.v0.3.0 status.updatedCSV")
+			subscription, err = fetchSubscription(crclient, subscription.GetNamespace(), subscription.GetName(), subscriptionHasCurrentCSV("example-operator.v0.3.0"))
+			Expect(err).Should(BeNil())
+		})
+	})
 })
 
 func objectRefToNamespacedName(ip *corev1.ObjectReference) types.NamespacedName {
